7f1772f8db3628b3e78594f902d38b3e101ee393
Zied Dhoukar <Snowman199@users.noreply.github.com>
2026-03-03 11:36:15 +0100

    Prepare Render deployment (gunicorn + render.yaml)

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, send_from_directory
 import random
+import os
 
 app = Flask(__name__, static_folder='.')
 
@@ -28,4 +29,5 @@ def static_files(path):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
